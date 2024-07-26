ed1d14c2e54ce0068942b75e8ad33bd27b65031a
DuvanLGomez <duvangomez2218@gmail.com>
2024-07-26 15:25:16 -0500

    Pedidos Restaurante

---
+++ pedidoRestaurante.py
@@ -0,0 +1,13 @@
+print("Buen dia, ¿Cual es tu nombre?")
+name = input()
+print("¡Bienvenido " + name + ", al restaurante buen gusto!")
+menu = {1: "Pizza napolitana", 2: "Osobuco", 3: "Pasta a la carbonara"}
+menu_str = "\n".join([f"{key}: {value}" for key, value in menu.items()])
+print("Para el dia de hoy tenemos el siguiente menu: " + "\n" + menu_str)
+print("¿Qué desea comer hoy? eliga una opción.")
+pedido = input()
+orden = menu[int(pedido)]
+time_entrega = 30 
+print("¿A que dirección quiere que lo llevemos?")
+adress = input()
+print("Perfecto "+ name + " su pedido de " + orden + " será entregado en " + str(time_entrega) + " minutos " + "en la dirección " + adress )

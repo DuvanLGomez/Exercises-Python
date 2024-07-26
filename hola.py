2838d44bee360ce9300fa3f3b98e74edadd0de29
DuvanLGomez <duvangomez2218@gmail.com>
2024-07-26 12:27:38 -0500

    actualizado

--- hola.py
+++ hola.py
@@ -1,5 +1,9 @@
 
 name = input("Hola, por favor ingresa tu nombre: ")
-print ("Bienvenido "+ name + " tu eres genial :D ")
+print("Bienvenido "+ name + " tu eres genial :D ")
 age = input("Ingresa tu edad: ")
-print ( name + " tu edad es: " + age)
+print(name + " tu edad es: " + age)
+def sum (age) -> int:
+   future_age = int(age) + 50
+   return future_age
+print ("Tu edad en 50 años será " + str(sum(age)) + " años")
